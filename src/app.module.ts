877aff340dbf579818edbfc68a1a2e092ed15b27
Onesco <Okonkwoonesmus5@gmail.com>
2022-10-08 23:48:14 +0100

    add user, Walle and Tranfer Module to the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,17 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { datasource } from './config/env.config';
 
+import { UserModule } from './app/user/user.module'
+import { WalletModule } from './app/wallet/wallet.module'
+import {TransferModule} from './app/transfer/transfer.module'
+
 @Module({
-  imports: [DatabaseModule.forRoot({config:{datasource}})],
+  imports: [
+    DatabaseModule.forRoot({config:{datasource}}),
+    UserModule,
+    WalletModule,
+    TransferModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
