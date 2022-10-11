be29ff1705fc4b834d9164cf256616550cb947e2
Onesco <Okonkwoonesmus5@gmail.com>
2022-10-11 03:41:53 +0100

    file clean up

--- src/app/user/dto/update-user.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
-import { IsNumberString } from 'class-validator';
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @IsNumberString()
-  id: number;
-}
